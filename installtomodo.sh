3aa0f054f8db2dc0c3a2a844955c528fabca0674
delacruzjardon <108693714+delacruzjardon@users.noreply.github.com>
2025-03-27 16:53:53 -0600

    Update installtomodo.sh

--- installtomodo.sh
+++ installtomodo.sh
@@ -2,10 +2,7 @@ mongod --shutdown --config /etc/mongod.conf
 sleep 10
 pip install tomodo
 apt-get -y install python3.10-venv
-
 tomodo provision sharded --shards 3
-
-
 python3 -m venv .
 source bin/activate
 python3 -m pip install pymongo 
